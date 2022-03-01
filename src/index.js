32305b6ce1ecb4ea458f699e9979d843e29cd99e
offtheclockk <88730624+offtheclockk@users.noreply.github.com>
2022-03-01 09:43:09 -0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import AppProviders from "./AppProviders";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <AppProviders>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </AppProviders>,
   document.getElementById('root')
 );
 
