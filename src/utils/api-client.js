87755f065bbc523d6a8de3cf11ccf39572413f45
offtheclockk <88730624+offtheclockk@users.noreply.github.com>
2022-03-01 14:46:05 -0800

    validations

--- src/utils/api-client.js
+++ src/utils/api-client.js
@@ -5,3 +5,24 @@ const client = axios.create();
 export async function authenticate() {
           return await client.get("/auth/login").then((res) => res.data.user);
 }
+
+export async function login(payload) {
+          await client.post("/auth/login", payload);
+          window.location.pathname = "/";
+}
+
+export async function signup(payload) {
+          await client.post("/auth/signup", payload);
+          window.location.assign("/settings/profile?redirected=true");
+}
+
+export async function logout() {
+          await client.get("/auth/logout");
+          window.location.pathname = "/";
+}
+
+export async function getNotifications() {
+          return await client
+                    .get(`/api/notifications`)
+                    .then((res) => res.data.notifications);
+}
